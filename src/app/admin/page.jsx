55ac98f21386d295ec0ddbe821a773be7636f091
DropBoost <dev@dropboost.it>
2025-08-24 20:44:58 +0200

    aggiunta logout ad admin

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -40,12 +40,24 @@ export default function PAGEAdmin() {
     checkRole()
   }, [router])
 
+    //LOGOUT
+    async function handleLogout() {
+    await supabase.auth.signOut()
+    router.push("/")
+    }  
+
   if (loading) return <p>Caricamento...</p>
 
   return (
     <>
       <h1>Benvenuto Admin!</h1>
       <p>Ruolo utente: {role}</p>
+                  <button
+                onClick={handleLogout}
+                className="py-1 px-2 w-fit text-xs bg-red-600 text-white rounded hover:bg-red-800"
+            >
+                {ICONoff} ESCI
+            </button>
     </>
   )
 }
